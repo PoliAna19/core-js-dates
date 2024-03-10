550d2464cee91494bca758cc0112c41ea1e41ca5
PoliAna19 <anastasiya.paliachok@gmail.com>
2024-03-10 17:41:08 +0400

    fifth task solved

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -86,8 +86,10 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const lastMonthDay = new Date(year, month, 0);
+  const lastMonthDate = lastMonthDay.getDate();
+  return lastMonthDate;
 }
 
 /**
